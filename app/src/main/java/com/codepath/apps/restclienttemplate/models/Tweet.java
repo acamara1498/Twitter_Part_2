1374120df7fd679686a15311277d158b4f59ef27
Amadou Camara <acamara@college.harvard.edu>
2017-07-04 12:26:55 -0700

    Fresh start to implement fragments in android

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,74 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import android.text.format.DateUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+/**
+ * Created by acamara on 7/3/17.
+ */
+
+public class Tweet {
+    //list out the attributes
+    public String body;
+    public long uid;// database id for the tweet
+    public User user;
+    public String createdAt;
+    public String relativeDate;
+
+    public Tweet(){
+    }
+
+    //deserialize the JSON
+    public static Tweet fromJSON(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+
+        //extract the values from JSON
+        tweet.body = jsonObject.getString("text");
+        tweet.uid = jsonObject.getLong("id");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.relativeDate = " • "+ getRelativeTimeAgo(tweet.createdAt); //TODO - Change the relative date into the correct format
+        tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+
+
+        return  tweet;
+    }
+
+    public static String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        relativeDate = relativeDate.replaceAll(" ago","");
+        relativeDate = relativeDate.replaceAll(" minutes","m");
+        relativeDate = relativeDate.replaceAll(" minute","m");
+        relativeDate = relativeDate.replaceAll(" hours","h");
+        relativeDate = relativeDate.replaceAll(" hour","h");
+        relativeDate = relativeDate.replaceAll(" seconds","s");
+        relativeDate = relativeDate.replaceAll(" second","s");
+        relativeDate = relativeDate.replaceAll(" days","d");
+        relativeDate = relativeDate.replaceAll(" day","d");
+
+
+
+
+
+
+
+        return relativeDate;
+    }
+}
